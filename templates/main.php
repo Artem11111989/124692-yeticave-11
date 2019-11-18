695188c12253d3a22371fe1134298172fc3cbaa8
Artem11111989 <55476040+Artem11111989@users.noreply.github.com>
2019-11-18 13:54:11 +0400

    Добавил в список лотов значения из базы данных

--- templates/main.php
+++ templates/main.php
@@ -9,7 +9,7 @@
 			$num = count($category);
 			while ($index < $num): ?>
             <li class = "promo__item promo__item--boards">
-                <a class = "promo__link" href = "pages/all-lots.html"><?= $category[$index]; ?></a>
+                <a class = "promo__link" href = "pages/all-lots.html"><?= $category[$index]; ?></a>   
 				<?php $index++; ?>
                 <?php endwhile; ?>
             </li>
@@ -20,6 +20,37 @@
             <h2>Открытые лоты</h2>
         </div>
         <ul class = "lots__list">
+		
+		
+		<?php /*$con = mysqli_connect("localhost", "root", "", "yeticave");
+if($con == false) {
+	print("Ошибка подключения: " . mysqli_connect_error());
+}
+else {
+	print("Соединение установлено");
+}
+
+mysqli_set_charset($con, "utf8");
+
+$sql = <<<SQL
+SELECT lots.id, lots.date_create, lots.name, lots.start_price, lots.picture, categories.name FROM lots
+JOIN categories ON lots.category_name = categories.id                                                                                                  
+WHERE lots.date_finish BETWEEN '2019-11-09' AND '2019-12-31'
+AND lots.date_create BETWEEN '2019-11-01 00:00' AND '2019-11-09 00:00'
+ORDER BY lots.date_create DESC
+SQL;
+
+$result = mysqli_query($con, $sql);
+
+if(!$result) {
+	$error = mysqli_error($con);
+	print("Ошибка MySQL: " . $error);
+}
+	
+$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);*/
+
+
+?>
             <!--заполните этот список из массива с товарами-->
 			<?php $lots = [["title" => "2014 Rossignol District Snowboard", "category" => "Доски и лыжи", "price" => "10999", "picture_URL" => "img/lot-1.jpg", "date_end" => "2019-12-24"],
                           ["title" => "DC Ply Mens 2016/2017 Snowboard", "category" => "Доски и лыжи", "price" => "159999", "picture_URL" => "img/lot-2.jpg", "date_end" => "2019-11-23"],
@@ -27,25 +58,28 @@
 						  ["title" => "Ботинки для сноуборда DC Mutiny Charocal", "category" => "Ботинки", "price" => "10999", "picture_URL" => "img/lot-4.jpg", "date_end" => "2019-11-18"],
 						  ["title" => "Куртка для сноуборда DC Mutiny Charocal", "category" => "Одежда", "price" => "7500", "picture_URL" => "img/lot-5.jpg", "date_end" => "2019-12-12"],
 						  ["title" => "Маска Oakley Canopy", "category" => "Разное", "price" => "5400", "picture_URL" => "img/lot-6.jpg", "date_end" => "2019-12-01"]]; ?>
-		    <?php foreach ($lots as $key => $value): ?>    
+		    <?php foreach ($lots as $pass => $arr) {
+				      foreach ($arr as $key => $value): ?>   
             <li class = "lots__item lot">
                 <div class = "lot__image">
                     <img src = "" width = "350" height = "260" alt = "">
                 </div>
                 <div class = "lot__info">
-                    <span class = "lot__category"><?= $value["category"]; ?></span>
-                    <h3 class = "lot__title"><a class = "text-link" href = "pages/lot.html"><?= $value["title"]; ?></a></h3>
+                    <span class = "lot__category"><?= $value["name_cat"]; ?></span> <!-- тут было = $value["category"] -->
+                    <h3 class = "lot__title"><a class = "text-link" href = "pages/lot.html"><?= $value["name_lots"]; ?></a></h3>   <!-- тут было = $value["title"] -->
                     <div class = "lot__state">
                         <div class = "lot__rate">
                             <span class = "lot__amount">Стартовая цена</span>
-						 	<span class = "lot__cost"><?= format_sum($value["price"]); ?></span>
+						 	<span class = "lot__cost"><?= format_sum($value["start_price"]); ?></span>   <!-- тут было = format_sum($value["price"]) -->
                         </div>
 						<div class = "lot__timer timer <?php if($hour == 0): ?>timer--finishing<?php endif ?>">  <!-- 3 ЛЕКЦИЯ 2 ЗАДАНИЕ!!! используем переменную, которой присвоили глобальное значение в функции time_remain для вывода дополнительного класса элемента, когда времени осталось меньше часа -->
-                            <?= time_remain($value["date_end"]); ?>                                              <!-- 3 ЛЕКЦИЯ 2 ЗАДАНИЕ!!! в цикле обхода массива лотов выводим количество часов и минут оставшихся до даты убирания лота с сайта-->
+                            <?= time_remain($value["date_finish"]); ?>        <!-- тут было = time_remain($value["date_end"]) -->                                      <!-- 3 ЛЕКЦИЯ 2 ЗАДАНИЕ!!! в цикле обхода массива лотов выводим количество часов и минут оставшихся до даты убирания лота с сайта-->
                         </div>
 					</div>
                 </div>
             </li>
-			<?php endforeach; ?>
+			<?php endforeach; 
+			           }?>
+			
         </ul>
     </section>
