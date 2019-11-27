cee3652c21bdc001840f7581cc16d39fed58581a
Artem11111989 <55476040+Artem11111989@users.noreply.github.com>
2019-11-27 08:31:05 +0400

    Добавил подключение массивов категорий и лотов функциями

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+
 require_once('functions.php');
 require_once('db.php');
+
+$category = get_all_categories();
+$lots = get_all_lots();
+
 $page_content = include_template('main.php', ['category' => $category, 'lots' => $lots]);
 $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Главная', 'category' => $category]);
+
 print($layout_content);
 
 ?>
