b091cf06c016cbaad7c5ae15093bc5c8eebeb921
Artem11111989 <55476040+Artem11111989@users.noreply.github.com>
2019-12-09 17:07:50 +0400

    Убрал подключение хедера

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ require_once('db.php');
 $category = get_all_categories();
 $lots = get_all_lots();
 
-$header = include_template('header.php', []);
+//$header = include_template('header.php', []);
 //$footer = include_template('footer.php', ['category' => $category]);
 
 //$lot_content = include_template('lot.php', ['category' => $category, 'header' => $header, 'footer' => $footer]);
 //print($lot_content);
 
 $page_content = include_template('main.php', ['category' => $category, 'lots' => $lots]);
-$layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Главная', 'category' => $category, 'header' => $header, 'footer' => $footer]);
+$layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Главная', 'category' => $category/*, 'header' => $header*/]);
 
 print($layout_content);
 
